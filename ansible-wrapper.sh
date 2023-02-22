6e8a975dc1286cd183a232d050efcd105372af9e
brett060102 <esampson@suse.com>
2023-02-22 22:00:42 +0000

    Update wrapper to support colns in path name and fix example
    podman and docker do not allow charters such as colon in the
    source path name. The work around is to create sym link and
    use the symlink as the source.
    
    Update example ansible to use become when checking if NetworkManager
    package is installed.

--- ansible-wrapper.sh
+++ ansible-wrapper.sh
@@ -6,5 +6,14 @@ if [[ $(id -ru) != "0" ]]; then
     KEED_USERID="--userns=keep-id"
 fi
 
-podman run  --security-opt label=disable -it -v ${PWD}:/work -v ${HOME}:${HOME} ${KEED_USERID} --rm ansible "$(basename "${0}")" "$@"
+# make symlinks for mount points
+# this needed to hand colons in file path names
+LINK_DIR=`mktemp -d -p /tmp`
+ln -s $(pwd)  ${LINK_DIR}/work
+ln -s ${HOME} ${LINK_DIR}/home
 
+podman run  --security-opt label=disable -it -v ${LINK_DIR}/work:/work -v ${LINK_DIR}/home:${HOME} ${KEED_USERID} --rm ansible "$(basename "${0}")" "$@"
+
+# clean up symlink area
+rm ${LINK_DIR}/work ${LINK_DIR}/home
+rmdir ${LINK_DIR}
